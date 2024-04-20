c8e3f75e6c086d6ad5511e6f85bddd1bb254706e
Gabriele Toselli <toselli.gabriele@gmail.com>
2024-04-20 13:58:41 +0200

    Revert "refactor(command-bus): useFactory instead of extends"
    This reverts commit 51847c188cf709ddccbbd0560b26bb45e0a5ba78.

--- src/command-bus/command-bus.module.ts
+++ src/command-bus/command-bus.module.ts
@@ -1,19 +1,18 @@
-import { Global, Logger, Module } from '@nestjs/common';
+import { Global, Injectable, Logger, Module } from '@nestjs/common';
 import { LocalCommandBus } from '@fizzbuds/ddd-toolkit';
 
 export const COMMAND_BUS = 'COMMAND_BUS';
 
+@Injectable()
+export class CommandBus extends LocalCommandBus {
+    constructor() {
+        super(new Logger(CommandBus.name));
+    }
+}
+
 @Global()
 @Module({
-    providers: [
-        {
-            provide: COMMAND_BUS,
-            useFactory: () => {
-                const logger = new Logger('CommandBus');
-                return new LocalCommandBus(logger);
-            },
-        },
-    ],
+    providers: [{ provide: COMMAND_BUS, useClass: CommandBus }],
     imports: [],
     exports: [COMMAND_BUS],
 })
