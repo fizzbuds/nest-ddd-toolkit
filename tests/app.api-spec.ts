efc39ffa21f2118905a5344907d00fb1556206a0
Gabriele Toselli <toselli.gabriele@gmail.com>
2024-06-13 12:32:37 +0200

    tests(app): remove fake config and load whole application module

--- tests/app.api-spec.ts
+++ tests/app.api-spec.ts
@@ -1,39 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppController } from '../src/app.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { envValidationSchema } from '../src/env-validation-schema';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (api)', () => {
     let app: INestApplication;
 
-    class ConfigServiceFake {
-        get(key: string) {
-            switch (key) {
-                default:
-                    return '';
-            }
-        }
-
-        getOrThrow(key: string) {
-            return this.get(key);
-        }
-    }
-
     beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [
-                ConfigModule.forRoot({
-                    isGlobal: true,
-                    validationSchema: envValidationSchema,
-                }),
-            ],
-            controllers: [AppController],
-        })
-            .overrideProvider(ConfigService)
-            .useClass(ConfigServiceFake)
-            .compile();
+            imports: [AppModule],
+        }).compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
