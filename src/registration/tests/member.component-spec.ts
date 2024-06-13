d5148c156a5d735f0f9efe32e3c83b9d187c9054
Gabriele Toselli <toselli.gabriele@gmail.com>
2024-06-13 15:27:52 +0200

    test(registration): add event publishing

--- src/registration/tests/member.component-spec.ts
+++ src/registration/tests/member.component-spec.ts
@@ -5,13 +5,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MemberAggregateRepo } from '../infrastructure/member.aggregate-repo';
 import { getMongoToken, MongoModule } from '@golee/mongo-nest';
 import { MongoClient } from 'mongodb';
-import { EventBusModule } from '../../event-bus/event-bus.module';
+import { EventBus, EventBusModule } from '../../event-bus/event-bus.module';
 import { MembersService } from '../members.service';
+import { MemberDeleted } from '../events/member-deleted.event';
+import { MemberRegistered } from '../events/member-registered.event';
 
 describe('Member Component Test', () => {
     let module: TestingModule;
     let mongodb: MongoMemoryReplSet;
     let membersService: MembersService;
+    let eventBus: EventBus;
 
     beforeAll(async () => {
         mongodb = await MongoMemoryReplSet.create({
@@ -28,7 +31,10 @@ describe('Member Component Test', () => {
         }).compile();
 
         membersService = module.get(MembersService);
+        eventBus = module.get(EventBus);
         await module.get(MemberAggregateRepo).init();
+
+        jest.spyOn(eventBus, 'publishAndWaitForHandlers');
     });
 
     afterEach(async () => {
@@ -46,6 +52,15 @@ describe('Member Component Test', () => {
             const { memberId } = await membersService.registerMember('John Doe');
 
             expect(await membersService.getMember(memberId)).toMatchObject({ name: 'John Doe' });
+        });
+
+        it('should publish member registered event', async () => {
+            const { memberId } = await membersService.registerMember('John Doe');
+
+            expect(eventBus.publishAndWaitForHandlers).toHaveBeenCalledWith({
+                name: MemberRegistered.name,
+                payload: { memberId, memberName: 'John Doe' },
+            });
         });
     });
 
@@ -66,6 +81,17 @@ describe('Member Component Test', () => {
             await membersService.deleteMember(memberId);
 
             expect(await membersService.getMember(memberId)).toBeNull();
+        });
+
+        it('should publish member deleted event', async () => {
+            const { memberId } = await membersService.registerMember('John Doe');
+
+            await membersService.deleteMember(memberId);
+
+            expect(eventBus.publishAndWaitForHandlers).toHaveBeenCalledWith({
+                name: MemberDeleted.name,
+                payload: { memberId },
+            });
         });
     });
 });
